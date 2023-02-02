20a0e7ec71d25fac65fe4437e5b30232b8f204df
Cosmin Tanislav <demonsingur@gmail.com>
2023-02-02 02:20:43 +0200

    techpack: display: apply HBM status on panel post enable
    Change-Id: I43a0fd3f5aa84ae60fd0ee2a1301a89e59a79f8a

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -4485,6 +4485,12 @@ static int dsi_panel_set_hbm_status(struct dsi_panel *panel,
 	return 0;
 }
 
+static int dsi_panel_apply_hbm_status(struct dsi_panel *panel)
+{
+	return dsi_panel_set_hbm_status(panel, panel->fod_hbm_enabled,
+					panel->hbm_enabled);
+}
+
 static int dsi_panel_set_dc_dimming_status(struct dsi_panel *panel, bool status)
 {
 	int rc;
@@ -6203,6 +6209,12 @@ int dsi_panel_post_enable(struct dsi_panel *panel)
 	}
 
 	PANEL_NOTIFY(PANEL_EVENT_DISPLAY_ON);
+
+	if (panel->hbm_enabled) {
+		rc = dsi_panel_apply_hbm_status(panel);
+		if (rc)
+			goto error;
+	}
 
 error:
 	mutex_unlock(&panel->panel_lock);
